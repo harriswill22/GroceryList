25bbf28fddb82e386f34409c9f72494d68d0f14a
Will Harris <Harriswill22@yahoo.com>
2018-11-05 18:43:14 -0500

    adds example calls to GroceryList

---
+++ index.js
@@ -0,0 +1,12 @@
+const GroceryList = require('./GroceryList')
+
+// GroceryList.getByID(3)
+// .then(results => {
+// console.log(results);
+// });
+
+
+GroceryList.getByID(2)
+.then(results => {
+console.log(results);
+});
