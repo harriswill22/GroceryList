5a061db11589d4006dd20d48faa032ffef924aea
Will Harris <Harriswill22@yahoo.com>
2018-11-07 14:42:31 -0500

    adds CRUD

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const GroceryList = require('./models/GroceryList')
 
 // GroceryList.getByID(3)
@@ -5,8 +6,82 @@ const GroceryList = require('./models/GroceryList')
 // console.log(results);
 // });
 
+// CRUD =============================
 
-GroceryList.getByID(2)
-.then(results => {
-console.log(results);
-});
+
+
+
+
+// CREATE ===========================
+
+
+// GroceryList.addItem('grapes', 5)
+// .then(result =>{
+//     console.log(result);
+    
+// })
+
+
+
+// RETRIEVE  ===========================
+
+
+// GroceryList.getByID(2)
+// .then(results => {
+// console.log(results);
+// });
+
+// GroceryList.getALL()
+// .then(results => {
+// console.log(results);
+// console.log('All the items');
+
+// });
+
+
+// UPDATE ===============================
+
+// GroceryList.getByID(8)
+// .then(item =>{
+//     item.updateItem('skittles')
+// .then(result =>{
+//     console.log(result);
+// })
+// })
+
+
+// GroceryList.getByID(4)
+// .then(item => {
+//     item.updatePurchased(true)
+//     .then(result =>{
+//     console.log(result);
+//     })
+// })
+
+// GroceryList.getByID(1)
+// .then(item => {
+//     item.markPurchased()
+//     .then(result =>{
+//         console.log(result);
+        
+//     })
+// })
+
+// GroceryList.getByID(2)
+// .then(item => {
+//     item.markPending()
+//     .then(result =>{
+//         console.log(result);
+        
+//     })
+// })
+
+//  DELETE ==================
+
+// GroceryList.deleteByID(6)
+// .then(result => {
+//     console.log(result.rowCount);
+//     console.log('Item deleted');
+// })
+
+
